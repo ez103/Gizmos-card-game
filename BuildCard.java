18d81bb8a6d171b67530a1ade58fa8a71454a391
ericz <ericz@10.0.0.88>
2023-10-29 15:16:22 -0500

    Flip flop. -eric

--- BuildCard.java
+++ BuildCard.java
@@ -1,13 +1,8 @@
 import java.awt.image.BufferedImage;
-import java.util.*;
 
 public class BuildCard extends Card {
 	public BuildCard(int cost, String costColor, int cardVictoryPoints, int tier, String category, BufferedImage image) {
 		super(cost, costColor, cardVictoryPoints, tier, category, image);
 		
-	}
-
-	public static void main(String[] args) {
-		System.out.println(" YES");
 	}
 }
--- GizmosRunner.java
+++ GizmosRunner.java
@@ -1,6 +1,10 @@
+import java.awt.image.BufferedImage;
+import java.util.*;
 
 public class GizmosRunner {
-	public static void main(String args) {
+	
+	
+	public static void main(String[] args) {
 		GizmosFrame gf = new GizmosFrame();
 		System.out.println(5);
 	}
