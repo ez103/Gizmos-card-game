dc3e69a882b127d723524e8351698466ba9e7a69
k1208278 <k1208278@T357554-1001L.slhs.katyisd.org>
2023-11-06 08:01:03 -0600

    images folder

--- X-Gamez-Premium-Free-copy/bin/FirstGraphic.java
+++ X-Gamez-Premium-Free-copy/bin/FirstGraphic.java
@@ -1,17 +1,18 @@
 import javax.swing.JFrame;
 import java.awt.*;
 import javax.swing.*;
-public class FirstGraphic extends JFrame{
+
+public class FirstGraphic extends JFrame {
     private static final int WIDTH = 1900;
     private static final int HEIGHT = 1000;
-    public FirstGraphic(String framename)
-{
-super(framename);
-setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-setSize(WIDTH,HEIGHT);
-add(new FirstGraphicPanel());
-setVisible(true);
-add(new twopanel());
-setVisible(true);
+    
+    public FirstGraphic(String framename) {
+    	super(framename);
+    	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	setSize(WIDTH,HEIGHT);
+    	add(new FirstGraphicPanel());
+    	setVisible(true);
+    	add(new twopanel());
+    	setVisible(true);
+	}
 }
-    }
