2741976cdba28ebb28fd79f2f8c0d640dd674eed
ez103 <148875289+ez103@users.noreply.github.com>
2023-11-07 08:08:28 -0600

    Update Player.java
    Signed-off-by: ez103 <148875289+ez103@users.noreply.github.com>

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,28 @@
+import javax.swing.*;
+import javax.imageio.*;
+import java.awt.*;
+import java.awt.image.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+import java.util.List;
 
 public class Player {
+	private BufferedImage belt;
+	private HashMap<String, ArrayList<Card>>;
+	private ArrayList<Marble>;
+	private String name;
+	private int playerNumber;
+	private int playerVictoryPoints;
+	private int marbleLimit;
+	private int fileLimit;
+	private int researchLimit;
+	private int x;
+	private int y;
 
+	public int getTotalVictoryPoints() {
+		return playerVictoryPoints;
+	}
+
+	
 }
