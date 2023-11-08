dbe528a1337b451c41c4d7b2508344beefc83c4f
ez103 <148875289+ez103@users.noreply.github.com>
2023-11-08 07:43:45 -0600

    Update Player.java
    Signed-off-by: ez103 <148875289+ez103@users.noreply.github.com>

--- src/Player.java
+++ src/Player.java
@@ -20,9 +20,31 @@ public class Player {
 	private int x;
 	private int y;
 
-	public int getTotalVictoryPoints() {
+	public int getTotalVictoryPoints() { // NOT FINISHED. need to include the points from all of the cards. DO this when card calsses are done.
 		return playerVictoryPoints;
 	}
 
-	
+	public void increaseMarbleLimit(int n) {
+		marbleLimit += n;
+	}
+
+	public void increaseFileLimit(int n) {
+		fileLimit += n;
+	}
+
+	public void increaseResearchLimit(int n) {
+		researchLimit += n;
+	}
+
+	public void setLocation(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
 }
