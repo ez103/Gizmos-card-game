270affdd5ec2bc93fa9bec5ba912b181114295d2
DevDoesstuf <77683118+DevDoesstuf@users.noreply.github.com>
2023-11-08 13:25:56 +0000

    wa wa

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.awt.image.*;
 import java.awt.*;
-
+//hi eric
 public class Card {
 	private int cost; // this is the cost to build the card -Eric
 	private String costColor; // this is the color of the marble used to build this card 
