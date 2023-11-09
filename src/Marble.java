40834d75c77e505977760ea85bac9e3f05a11d0f
ez103 <148875289+ez103@users.noreply.github.com>
2023-11-09 13:48:12 +0000

    update image names and marble class.

--- src/Marble.java
+++ src/Marble.java
@@ -1,4 +1,32 @@
 
 public class Marble {
-// shit
+    private String color;
+    private BufferedImage marbleImage;
+    private int x;
+    private int y;
+
+    public Marble(String color) {
+        if (color = "g")
+    }
+
+    public void setLocation(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+
+    private void convert(String color) {
+        this.color = color;
+    }
+    private String getColor() {
+        return color;
+    }
+    private BufferedImage getImage() {
+        return image;
+    }
 }
