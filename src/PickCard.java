2f84db26d130cfac49a3857d8473aeaa1e30d3cd
ez103 <148875289+ez103@users.noreply.github.com>
2023-11-10 13:43:48 +0000

    pickcard changes

--- src/PickCard.java
+++ src/PickCard.java
@@ -1,5 +1,8 @@
 
-public class PickCard {
+public class PickCard extends Card {
     private ArrayList<String> pickedColors;
 
+    public PickCard(int cost, String costColor, int cardVictoryPoints, int tier, String category, BufferedImage image) {
+        super(cost, costColor, cardVictoryPoints, tier, category, image);
+	}
 }
