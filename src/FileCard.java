1613f03cdd5770548d5d2e128ad3063112cd8e49
ez103 <148875289+ez103@users.noreply.github.com>
2023-11-10 13:38:05 +0000

    updates on Card sublclass and shit

--- src/FileCard.java
+++ src/FileCard.java
@@ -1,10 +1,18 @@
 
-public class FileCard {
-    private int chooseMarble;
-    private int randomMarble;
-    private int playerVictoryPoints;
+public class FileCard extends Card {
+    private int chooseMarble; // how many of the 6 visible marbles you can pick
+    private int randomMarble; // how many random marbles can you draw
+    private int playerVictoryPoints; // how many player victory points this will give the player. NOT card victory points; thats in the Card class.
     
-    public FileCard (int a, int b){
+    public FileCard(int cost, String costColor, int cardVictoryPoints, int tier, String category, BufferedImage image) {
+        super(cost, costColor, cardVictoryPoints, tier, category, image);
+	}
 
+    public void setFileCard(int chooseMarble, int randomMarble, int playerVictoryPoints) {
+        this.chooseMarble = chooseMarble;
+        this.randomMarble = randomMarble;
+        this.playerVictoryPoints = randomMarble;
     }
+
+
 }
