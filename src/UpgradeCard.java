d4e0a0a3177ba723e6564fa2efa33e5bb1fc92b3
ez103 <148875289+ez103@users.noreply.github.com>
2023-11-14 13:45:41 +0000

    upgrade card pivs and rudimentary constructor;

--- src/UpgradeCard.java
+++ src/UpgradeCard.java
@@ -1,4 +1,21 @@
+import java.util.*;
 
-public class UpgradeCard {
+public class UpgradeCard extends Card {
+    private int marbleLimitIncrease;
+    private int fileLimitIncrease;
+    private int researchLimitIncrease;
 
+    private boolean negateFile;
+    private boolean negateResearch;
+    
+    private int tierTwoDiscount;
+    private int buildFromFileDiscount;
+    private int buildFromResearchDiscount;
+
+    private boolean isVpMarbles;
+    private boolean isVpPlayerVp;
+
+    public UpgradeCard(int cost, String costColor, int cardVictoryPoints, int tier, String category, BufferedImage image) {
+        super(cost, costColor, cardVictoryPoints, tier, category, image);
+	}
 }
