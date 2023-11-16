36f927e6ff1d88d84a295d98dff4e62f69c3e160
ez103 <148875289+ez103@users.noreply.github.com>
2023-11-16 14:12:53 +0000

    added 2 cards.

--- src/Deck.java
+++ src/Deck.java
@@ -5,7 +5,12 @@ import java.awt.*;
 public class Deck {
     public ArrayList<Card> cards; 
     
+    // parameter s must be in this format
+    // : [cost][lowercase first letter of color][cardVP][tier][category: lowercase first letter]
     public Deck() {
-        
+        Card r1p1_1 = new FileCard("1a11f");
+        cards.add(r1p1_1);
+
+        Card r1p1_2 = new FileCard("1u11f");
     }
 }
