9c0b28312b6d93eb011e1a9ef7746ddc2ae78a57
ericz <ericz@10.0.0.88>
2023-11-16 21:14:57 -0600

    deck changes more moree

--- src/Deck.java
+++ src/Deck.java
@@ -1,7 +1,10 @@
 import java.util.*;
+
+import javax.imageio.ImageIO;
+
 import java.awt.image.*;
+import java.io.IOException;
 import java.awt.*;
-import java.image.ImageIO.*;
 
 public class Deck {
     public ArrayList<Card> cards; 
@@ -9,9 +12,17 @@ public class Deck {
     // parameter s must be in this format
     // : [cost][lowercase first letter of color][cardVP][tier][category: lowercase first letter]
     public Deck() {
-        Card r1p1_1 = new FileCard("1a11f", ImageIO.read(Deck.class.getResource("/cardImages/r1p1_1.png")));
-        cards.add(r1p1_1);
-
-        Card r1p1_2 = new FileCard("1u11f");
+    	
+    	try {
+    		Card r1p1_1 = new FileCard("1a11f", ImageIO.read(Deck.class.getResource("/cardImages/r1p1_1.png")));
+    		cards.add(r1p1_1);
+    		
+    		Card r1p1_2 = new FileCard("1u11f", ImageIO.read(Deck.class.getResource("/cardImages/r1p1_2.png")));
+    		cards.add(r1p1_2);
+    	}
+    	catch (Exception E) {
+    		System.out.println("image exception CARDS DECK rank 1");
+    	}
+        
     }
 }
