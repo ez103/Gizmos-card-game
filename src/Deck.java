dfe66c147bfc36bd1ff90ebed2a5a60022f5010c
ez103 <148875289+ez103@users.noreply.github.com>
2023-11-17 03:01:55 +0000

    testing card deck

--- src/Deck.java
+++ src/Deck.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.awt.image.*;
 import java.awt.*;
+import java.image.ImageIO.*;
 
 public class Deck {
     public ArrayList<Card> cards; 
@@ -8,7 +9,7 @@ public class Deck {
     // parameter s must be in this format
     // : [cost][lowercase first letter of color][cardVP][tier][category: lowercase first letter]
     public Deck() {
-        Card r1p1_1 = new FileCard("1a11f");
+        Card r1p1_1 = new FileCard("1a11f", ImageIO.read(Deck.class.getResource("/cardImages/r1p1_1.png")));
         cards.add(r1p1_1);
 
         Card r1p1_2 = new FileCard("1u11f");
